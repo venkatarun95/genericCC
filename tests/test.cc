57980784122ad5e53f4ff78a6dfbafb9b4df2b66
Venkat Arun <venkatarun95@gmail.com>
2016-02-24 12:57:23 +0530

    Unit tests module now compares stdout to template file

--- tests/test.cc
+++ tests/test.cc
@@ -1,6 +1,7 @@
 #include "unit-test.h"
 #include "unit-test-window.h"
 
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <tuple>
@@ -12,8 +13,28 @@ int main() {
 	vector< pair<UnitTest*, string> > unit_tests;
 	unit_tests.push_back(make_pair(new UnitTestWindow(), string("Window")));
 
+	std::streambuf *coutbuf = cout.rdbuf(); //save old buf
 	for (const auto& test : unit_tests) {
+		ofstream out(string("tests/output/") + test.second + string(".output"));
+		cout.rdbuf(out.rdbuf());
 		bool res = test.first->run_tests();
+		cout.rdbuf(coutbuf);
+		out.close();
+
+		// Check if output was as expected
+		ifstream actual(string("tests/output/") + test.second + string(".output"));
+		ifstream exp(string("tests/expected_output/") + test.second + string(".output"));
+		while (!actual.eof() && !exp.eof()) {
+			char a, e;
+			actual >> a; exp >> e;
+			if (a != e) {
+				res = false;
+				break;
+			}
+		}
+		if (!actual.eof() || !exp.eof())
+			res = false;
+
 		if (res)
 			cout << "Unit Test Passed: '" << test.second << "'." << endl;
 		else
